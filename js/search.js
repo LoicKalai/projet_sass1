4b203dec05fc7d04660d6c5b0678c94aa855da58
Loïc Kalai <lkalaidjoglou@PORT202109-050.localdomain>
2022-02-04 11:20:21 +0100

    [import]
    import icon et changement de dossier

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
-let btnsearch = document.querySelector('.btnsearch');
-let search = document.getElementById('.barrech');
-search.addEventListener('click', function(e){
-    e.preventDefault
-    
 
-})
+   
+
+   
+    
+    
+  
